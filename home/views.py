52fe9d4cf4dd04820b2e4f2407cf05c209a2bae1
Sahin <xahinds2@gmail.com>
2023-07-14 00:13:14 +0530

    login url removed

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
 
 
-@login_required(login_url='login/')
+@login_required
 def home(request):
     return render(request, 'home.html')
 
