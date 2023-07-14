0089769f913db7cbdbcc1cfb18a38929da78ad8b
Sahin <xahinds2@gmail.com>
2023-07-14 10:04:17 +0530

    home page added

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
 
 
-@login_required
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'isLogin': request.user.username
+    }
+    return render(request, 'home.html', context)
 
 
 def custom_login(request):
